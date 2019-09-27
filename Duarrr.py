2bb19e83a92b1295a3378f5ed189a33b7bfce14b
Rehan Adi <rehanadi457@gmail.com>
2019-09-27 21:39:22 +0700

    Ini file mainnya

---
+++ Duarrr.py
@@ -0,0 +1,11 @@
+Hygiene = 0
+Energy = 10
+Fun = 0
+print("SELAMAT DATANG DI PERMAINAN DE SIMS DUARRRR!!!!")
+print("----------------------------------------------------")
+print()
+print("Ini adalah atribut terkini anda")
+print("")
+print("1. Hygiene = " + str(Hygiene))
+print("2. Energy = " + str(Energy))
+print("3. Fun = " + str(Fun))
